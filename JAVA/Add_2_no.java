84ad01a27859fe5e475595b022cf09fd20b51ba4
Saurabh Kumar <sk0008861@gmail.com>
2023-05-05 23:20:41 +0530

    Second

---
+++ JAVA/Add_2_no.java
@@ -0,0 +1,8 @@
+public class Add_2_no {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 8;
+        int num3 = num1+num2;
+        System.out.println("Addition of num1 and num2 : "+num3);
+    }
+}
