963b025d94e75571cabbd1b8d9272515dbdb06c7
Saurabh Kumar <77958948+CodewithSaurabh@users.noreply.github.com>
2023-05-06 19:00:18 +0530

    second

---
+++ Print_and_line.java
@@ -0,0 +1,6 @@
+public class Print_and_line {
+    public static void main(String[] args) {
+        System.out.println("Martin Fowler");    //Message and then new line
+        System.out.print("Any fool can write code that a computer can understand.\nGood programmers write code that humans can understand.");     //Message and then new line via \n
+    }
+}
