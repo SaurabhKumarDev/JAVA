0d1bff632009848b97999faaabb29094d1c12cd5
Saurabh Kumar <77958948+CodewithSaurabh@users.noreply.github.com>
2023-05-06 17:28:48 +0530

    Delete JAVA directory

--- JAVA/Add_2_no.java
+++
@@ -1,8 +0,0 @@
-public class Add_2_no {
-    public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 8;
-        int num3 = num1+num2;
-        System.out.println("Addition of num1 and num2 : "+num3);
-    }
-}
