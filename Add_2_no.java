2a4f739fbb9970d12173889431d291a67a43a18e
Saurabh Kumar <77958948+CodewithSaurabh@users.noreply.github.com>
2023-05-06 18:45:42 +0530

    Delete Add_2_no.java

--- Add_2_no.java
+++
@@ -1,8 +0,0 @@
-public class Add_2_no {
-    public static void main(String[] args) {
-        int num1 = 5;
-        int num2 = 6 ;
-        int sum = num1 + num2;
-        System.out.println(sum);
-    }
-}
