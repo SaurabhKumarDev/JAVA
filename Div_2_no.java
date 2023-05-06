a4a4774f7e55b560bed46c688957026af29575c5
Saurabh Kumar <77958948+CodewithSaurabh@users.noreply.github.com>
2023-05-06 19:05:53 +0530

    Arithmetic Operations
    A program has been designed to perform arithmetic operations such as addition, subtraction, multiplication, and division using predefined values, and then printing the results at terminal.

---
+++ Div_2_no.java
@@ -0,0 +1,8 @@
+public class Div_2_no {
+    public static void main(String[] args) {
+        int num1 = 6;
+        float num2 = 5;   //It return the decimal value
+        float num3 = num1/num2; //Return float value
+        System.out.println(num3);
+    }
+}
---
+++ Sub_2_no.java
@@ -0,0 +1,8 @@
+public class Sub_2_no {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 8;
+        int num3 = num1 - num2;
+        System.out.println(num3);
+    }
+}
