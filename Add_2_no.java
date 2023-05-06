b3dc3c6a16f9d978a6827eb165adbaa9459091cf
Saurabh Kumar <77958948+CodewithSaurabh@users.noreply.github.com>
2023-05-06 18:42:29 +0530

    Beginner

---
+++ Add_2_no.java
@@ -0,0 +1,8 @@
+public class Add_2_no {
+    public static void main(String[] args) {
+        int num1 = 5;
+        int num2 = 6 ;
+        int sum = num1 + num2;
+        System.out.println(sum);
+    }
+}
