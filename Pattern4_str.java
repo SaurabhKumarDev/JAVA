2b28584b9b203d3429ad8aa21fd5d89c025abe0e
Saurabh Kumar <77958948+CodewithSaurabh@users.noreply.github.com>
2023-05-23 11:01:35 +0530

    For Loop
    Pattern of Strings
    
    Pattern 4
    
    Output :
        A 
       B B
     C C C
    D D D D
    E E E E E

---
+++ Pattern4_str.java
@@ -0,0 +1,17 @@
+public class Pattern4_str {
+    public static void main(String[] args) {
+        char ch='A';
+        for(int i=1; i<=5; i++) {
+            for(int j=5; j>i; j--) {
+                System.out.print(" ");
+                
+            }
+            for(int j=1; j<=i; j++) {
+                System.out.print(ch +" ");
+            }
+            System.out.println();
+            ch++;
+        }
+        System.out.println();
+    }
+}
