53a814c815ad5236ae74c99221c2c2d1bee35b02
Saurabh Kumar <77958948+CodewithSaurabh@users.noreply.github.com>
2023-05-23 12:10:27 +0530

    For Loop
    Problem 1 
    
    Output :- It will never stop loop because it does not have any ending point
    Saurabh
    Saurabh
    Saurabh
    Saurabh
    Saurabh
    Saurabh
     : : : : : : :
     : : : : : : :
    Saurabh
     : : : : : : :
     : : : : : : :
    Saurabh

---
+++ Problem_1.java
@@ -0,0 +1,9 @@
+public class Problem_1 {
+    public static void main(String[] args) {
+        for( ; ; ) {
+            System.out.println("Saurabh");
+        }
+    }
+}
+
+//This Program doesn't have any starting point and ending point so loop doesn't stop
