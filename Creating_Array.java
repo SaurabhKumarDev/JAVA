6ae1d916c7e6839a30cf083a9f6002b6793b2565
Saurabh Kumar <134705606+SaurabhKumarDev@users.noreply.github.com>
2023-05-29 19:58:24 +0530

    two methods
    Here, I present two approaches for printing or utilizing the array, as illustrated in the provided code snippets.

---
+++ Creating_Array.java
@@ -0,0 +1,15 @@
+public class Creating_Array {
+    public static void main(String[] args) {
+        
+        //Creating an array and inserting their value 
+        int array[] = new int[4];
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+
+        //Second method
+        int a[] = {1,2,3,4};
+        System.out.println(a[3]);
+    }
+}
